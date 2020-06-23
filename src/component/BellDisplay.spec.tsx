018e3d0f03a8b1da22b662c3029004b22af270a1
Coteh <3276350+Coteh@users.noreply.github.com>
2020-06-23 18:45:30 -0400

    Improve BellDisplay tests

--- src/component/BellDisplay.spec.tsx
+++ src/component/BellDisplay.spec.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { render, getByRole, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import BellDisplay from './BellDisplay';
+
+import MobileCheck from '../util/MobileCheck';
+
+jest.mock('../util/MobileCheck');
 
 describe('BellDisplay component', () => {
   test('should render without crash', () => {
@@ -28,6 +32,11 @@ describe('BellDisplay component', () => {
     expect(elem.getAttribute('alt')).toBe(expectedAlt);
   });
   test('should display tooltip with bell units and bell amount on mouse hover (desktop)', () => {
+    const mockNonMobile = jest.fn();
+    mockNonMobile.mockReturnValue(false);
+
+    MobileCheck.isMobile = mockNonMobile;
+
     const expectedAmount: number = 12;
     const expectedUnits: number = 1000;
 
@@ -45,6 +54,11 @@ describe('BellDisplay component', () => {
     );
   });
   test('should display tooltip with quantity and bell amount on tap (mobile)', () => {
+    const mockMobile = jest.fn();
+    mockMobile.mockReturnValue(true);
+
+    MobileCheck.isMobile = mockMobile;
+
     const expectedAmount: number = 12;
     const expectedUnits: number = 1000;
 
@@ -54,12 +68,15 @@ describe('BellDisplay component', () => {
 
     const topElem = getByText(expectedAmount.toString());
     expect(topElem).toBeTruthy();
-    // TODO change user agent to mobile but desktop can still tap so may not be required
+
     fireEvent.click(topElem);
     const tooltip = getByText(`${expectedAmount} stacks of`);
     expect(tooltip).toBeTruthy();
     expect(tooltip.textContent).toBe(
       `${expectedAmount} stacks of ${expectedUnits} bells`,
     );
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 });
