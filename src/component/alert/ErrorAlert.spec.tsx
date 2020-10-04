36023c67e994db099b7246edf5e583f8ccb75c30
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2020-10-04 09:01:34 +0200

    Made some tests to the ErrorAlert component.

---
+++ src/component/alert/ErrorAlert.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import ErrorAlert from './ErrorAlert';
+
+describe('ErrorAlert component', () => {
+  test('should render without crash', () => {
+    render(<ErrorAlert />);
+  });
+
+  test('should render the message we pass', () => {
+    const message = 'Test error message';
+
+    const { container } = render(<ErrorAlert message={message} />);
+
+    const messageElement = container.querySelector('div');
+    expect(messageElement).toHaveTextContent(message);
+  });
+});
